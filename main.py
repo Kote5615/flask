0d3a8e9b9c397de16ce3e0149c52473478361edb
humbleOstrich <118448454+humbleOstrich@users.noreply.github.com>
2023-03-26 18:19:34 +0500

    Добавлен поиск

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from data import db_session
 from data.users import User
 from data.books import Book
 from data.user_purchases import Purchase
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
-from forms.user import RegisterForm, LoginForm
+from forms.user import RegisterForm, LoginForm, SearchForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -77,12 +77,15 @@ def logout():
 @app.route("/")
 @app.route("/profile")
 def profile():
+    # form = SearchForm()
+    # if form.validate_on_submit():
+    #     return redirect('/search/<{}>/<{}>'.format(form.name.data, form.author.data))
     return render_template('profile.html')
 
 
-@app.route('/add_to_basket/<index>/', methods=['POST'])
+@app.route('/add_to_basket/<index>/<author>/<name>', methods=['POST'])
 @login_required
-def add_to_basket(index):
+def add_to_basket(index, author, name):
     db_sess = db_session.create_session()
     book = db_sess.query(Book).get(index)
     book.quantity = book.quantity - 1
@@ -96,14 +99,32 @@ def add_to_basket(index):
     )
     db_sess.add(purchase)
     db_sess.commit()
-    return redirect("/search")
+    return search_results(author, name)
 
 
-@app.route('/search', methods=['GET', 'POST'])
-def search():
+@app.route("/search", methods=['GET', 'POST'])
+def search_form():
+    form = SearchForm()
+    if form.validate_on_submit():
+        print(form.name.data, form.author.data)
+        # return redirect('/results/<{}>/<{}>'.format(form.author.data, form.name.data))
+        return search_results(form.author.data, form.name.data)
+    return render_template('search.html', form=form)
+
+
+@app.route('/results/', methods=['GET', 'POST'])
+def search_results(author, name):
     db_sess = db_session.create_session()
-    books = db_sess.query(Book).all()
-    return render_template("search.html", books=books)
+    book = db_sess.query(Book).filter((Book.name == name),
+    (Book.author == author)).first()
+    print(book)
+    if not book:
+        books = db_sess.query(Book).all()
+        return render_template("results.html", title="Ничего не найдено. \n"
+                                                     "Ознакомьтесь с нашим каталогом", books=books,
+                               author=author, name=name)
+    return render_template("result.html", title="Результаты поиска", item=book,
+                           author=author, name=name)
 
 
 def main():
